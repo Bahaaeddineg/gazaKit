14a65045e0299bd0daad83d31e9488e7821bd961
Bahaaeddineg <121982483+Bahaaeddineg@users.noreply.github.com>
2023-11-03 12:28:09 +0100

    theme + extentions

---
+++ gazakit/lib/src/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const kPrimaryColor = Color(0xff100B20);
+  static const kWhite = Colors.white;
+  static const kBlack = Colors.black;
+  static const kGrey = Colors.grey;
+  static const kRed = Colors.red;
+  static const kGreen= Colors.green;
+}
